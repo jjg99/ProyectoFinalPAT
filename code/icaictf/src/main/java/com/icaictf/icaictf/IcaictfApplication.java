ce49626e8b27ec36f6fde8a5ff96c4145f7cdd28
jjg99 <jjarauta@alu.comillas.edu>
2021-03-22 18:36:51 +0100

    Creación del proyecto Spring

---
+++ code/icaictf/src/main/java/com/icaictf/icaictf/IcaictfApplication.java
@@ -0,0 +1,13 @@
+package com.icaictf.icaictf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcaictfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcaictfApplication.class, args);
+	}
+
+}
---
+++ code/icaictf/src/test/java/com/icaictf/icaictf/IcaictfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icaictf.icaictf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcaictfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
