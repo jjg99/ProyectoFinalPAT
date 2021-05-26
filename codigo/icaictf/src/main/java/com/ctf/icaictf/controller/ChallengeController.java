ef525eb9de52e104073525ae53122f8992b9b896
J0nan <jagilcarrera@gmail.com>
2021-05-27 00:34:37 +0200

    Cambio de end point
    Desafios #15

--- codigo/icaictf/src/main/java/com/ctf/icaictf/controller/ChallengeController.java
+++ codigo/icaictf/src/main/java/com/ctf/icaictf/controller/ChallengeController.java
@@ -9,10 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +22,6 @@ public class ChallengeController {
     @Autowired
     private ChallengeService challengeService;
     
-    // @PostMapping("/challenge") TODO Ponemos este mejor? 
     @PostMapping("/new")
     public ResponseEntity<String> createChallenge(@RequestBody @Valid Challenge challenge) {
         // System.out.println(challenge.getData().get("archivos"));
@@ -34,8 +33,8 @@ public class ChallengeController {
         }
     }
 
-    @GetMapping("/challenge")
-    public ResponseEntity<Object> getChallenge(@RequestParam Integer id) {
+    @GetMapping("/challenge/{id}")
+    public ResponseEntity<Object> getChallenge(@PathVariable ("id") Integer id) {
         Challenge challenge = challengeService.getChallengeById(id);
         if (challenge!=null){
             return new ResponseEntity<>(challenge, HttpStatus.OK);
